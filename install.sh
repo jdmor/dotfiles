2d60cecbb6629362917119db7e334bc44df15af2
Jonathan Morrissey <jonathan@pixieset.com>
2016-09-25 16:11:01 -0700

    Add reminder for required packages

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ ln -sfv ~/.dotfiles/.bashrc
 ln -sfv ~/.dotfiles/.git-completion.bash
 ln -sfv ~/.dotfiles/.vimrc
 ln -sfv ~/.dotfiles/.ctags
+
+# Install the following packages using homebrew
+# vim
+# ctags
+# the_silver_searcher / ag
