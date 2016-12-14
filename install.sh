63db40697170eff20b22c54916efd8a336af4273
Jonathan Morrissey <jona.morrissey@gmail.com>
2016-12-13 22:31:44 -0800

    Create sym link for tmux config file

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ ln -sfv ~/.dotfiles/.bashrc ~/.bashrc
 ln -sfv ~/.dotfiles/.git-completion.bash ~/.git-completion.bash
 ln -sfv ~/.dotfiles/.vimrc ~/.vimrc
 ln -sfv ~/.dotfiles/.ctags ~/.ctags
+ln -sfv ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 # Install the following packages using homebrew
 # vim
 # ctags
 # ack
 # the_silver_searcher / ag
+# tmux
